bba5047b6fe214595f2e2df75dfa17c478170c01
Kurt Ostergaard <yodude@gmail.com>
2023-09-08 22:47:47 -0400

    Changed SPdata to df and nothing more.

--- EMA.R
+++ EMA.R
@@ -33,7 +33,7 @@ sourceCpp(
     ")
 
 # load the data file  # https://www.seykota.com/tribe/TSP/resources/SP----C.csv
-SPdata <- read_csv("SP----C.csv",
+df <- read_csv("SP----C.csv",
      col_names = c("date", "open", "high", "low", "close", "volume", "open_int"),
      col_types = list(
        date = col_date(format = "%Y%m%d"),
@@ -46,14 +46,14 @@ SPdata <- read_csv("SP----C.csv",
      )
 
 # calculate ATR, average true range, for risk sizing the trades
-SPdata <- SPdata |>
+df <- df |>
   select(date:close) |>
   mutate(hc_yest = high - lag(close),
          lc_yest = lag(close) - low,
          range = high - low) |>
   mutate(ATR = pmax(range, hc_yest, lc_yest, na.rm = TRUE))
-SPdata$atr_EMA <- ewmaRcpp(SPdata$ATR, 20)
-SPdata_orig <- SPdata
+df$atr_EMA <- ewmaRcpp(df$ATR, 20)
+df_orig <- df
 
 # profvis({
 
@@ -66,21 +66,21 @@ colnames(results) <- unlist(str_split("j, slow_lag, fast_lag,
             ICAGR, drawdown, bliss, lake, end_val, trade_test", ", "))
 
 for (j in seq_len(nrow(runs))) {
-  SPdata <- SPdata_orig
+  df <- df_orig
   # calculate exponential moving averages for low pass filter trade signals
   slow_lag <-  runs$slow[j]  # 15/150 ends with $3,303,931.25
   fast_lag <-  runs$fast[j]  # 85/325 leads to $13,551,818.75
-  SPdata$slow <- ewmaRcpp(SPdata$close, slow_lag)
-  SPdata$fast <- ewmaRcpp(SPdata$close, fast_lag)
+  df$slow <- ewmaRcpp(df$close, slow_lag)
+  df$fast <- ewmaRcpp(df$close, fast_lag)
 
-  SPdata <- SPdata |>           # EMA crossover
+  df <- df |>           # EMA crossover
     mutate(cross = fast - slow,
            on = ifelse(cross > 0 & lag(cross) < 0, 1, 0),
            off = ifelse(cross < 0 & lag(cross) > 0, -1, 0))
-  SPdata <-  slice(SPdata, 26:n()) # drop first 25 rows to 'warm up' EMA
+  df <-  slice(df, 26:n()) # drop first 25 rows to 'warm up' EMA
 
-  if(SPdata$cross[1] > 0) SPdata$on[1] <- 1    # trade signal details
-  SPdata <- SPdata |>
+  if(df$cross[1] > 0) df$on[1] <- 1    # trade signal details
+  df <- df |>
     mutate( signal = on + off,
             buy_date = ifelse(on == 1, as_date(lead(date)), 0),
             buy_price = ifelse(on == 1, (lead(open) + lead(high))/2, 0),
@@ -89,57 +89,57 @@ for (j in seq_len(nrow(runs))) {
             sell_price = ifelse(off == -1, (lead(open) + lead(low))/2, 0))
 
   # Close out last trade if long when data file runs out
-  if (SPdata$cross[nrow(SPdata)] > 0) {
-    SPdata$off[nrow(SPdata)] <- -1
-    SPdata$signal[nrow(SPdata)] <- -1
-    SPdata$sell_date[nrow(SPdata)] <- SPdata$date[nrow(SPdata)]
-    SPdata$sell_price[nrow(SPdata)] <- SPdata$close[nrow(SPdata)]
+  if (df$cross[nrow(df)] > 0) {
+    df$off[nrow(df)] <- -1
+    df$signal[nrow(df)] <- -1
+    df$sell_date[nrow(df)] <- df$date[nrow(df)]
+    df$sell_price[nrow(df)] <- df$close[nrow(df)]
   }
 
   # set up pnl & equity columns
-  start_value <- 1e6 ; SPdata$closed_pnl <- 1e6 ; SPdata$highwater <- 1e6
-  SPdata$open_pnl <- 0 ; SPdata$equity <- 0 ; SPdata$water <- 0
-  SPdata$drawdown <- 0 ; buy_amount <- 0 ; buy_price <- 0
+  start_value <- 1e6 ; df$closed_pnl <- 1e6 ; df$highwater <- 1e6
+  df$open_pnl <- 0 ; df$equity <- 0 ; df$water <- 0
+  df$drawdown <- 0 ; buy_amount <- 0 ; buy_price <- 0
   heat <- 0.1  # aka risk budget, is 10% of the book equity for each trade
   ATR_multiplier <- 5 # dynamic risk sizing based on current volatility via ATR
 
   # calculate trade pnl i=1
-  if(SPdata$signal[1] == 1){
-    buy_amount = plyr::round_any((SPdata$closed_pnl[[1]] *
-                heat) / (ATR_multiplier * SPdata$atr_EMA[[1]]), 250, f = round)
-    SPdata$buy_amount[1] = buy_amount
-    buy_price = SPdata$buy_price[1]
+  if(df$signal[1] == 1){
+    buy_amount = plyr::round_any((df$closed_pnl[[1]] *
+                heat) / (ATR_multiplier * df$atr_EMA[[1]]), 250, f = round)
+    df$buy_amount[1] = buy_amount
+    buy_price = df$buy_price[1]
   }
   # calculate trade pnl i>1
-  for (i in seq2(2, nrow(SPdata))){
-    SPdata$closed_pnl[i] = SPdata$closed_pnl[i-1]
-    if(SPdata$signal[i] == -1){
-      SPdata$closed_pnl[i] = SPdata$closed_pnl[i] +
-        (SPdata$sell_price[i] - buy_price) * buy_amount
+  for (i in seq2(2, nrow(df))){
+    df$closed_pnl[i] = df$closed_pnl[i-1]
+    if(df$signal[i] == -1){
+      df$closed_pnl[i] = df$closed_pnl[i] +
+        (df$sell_price[i] - buy_price) * buy_amount
       buy_amount = 0
-      SPdata$open_pnl[i] = 0
-    } else if(SPdata$signal[i] == 1){
-      buy_amount = plyr::round_any((SPdata$closed_pnl[[i]] *
-              heat) / (ATR_multiplier * SPdata$atr_EMA[[i]]), 250, f = round)
-      SPdata$buy_amount[i] = buy_amount
-      buy_price = SPdata$buy_price[i]
+      df$open_pnl[i] = 0
+    } else if(df$signal[i] == 1){
+      buy_amount = plyr::round_any((df$closed_pnl[[i]] *
+              heat) / (ATR_multiplier * df$atr_EMA[[i]]), 250, f = round)
+      df$buy_amount[i] = buy_amount
+      buy_price = df$buy_price[i]
     } else {
-      SPdata$open_pnl[i] = (SPdata$close[i] - buy_price) * buy_amount
+      df$open_pnl[i] = (df$close[i] - buy_price) * buy_amount
     }
-    SPdata$equity[i] <- SPdata$open_pnl[i] + SPdata$closed_pnl[i]
-    SPdata$highwater[i] <- pmax(SPdata$equity[i], SPdata$highwater[i-1])
-    SPdata$water[i] <- SPdata$highwater[i] - SPdata$equity[i]
-    SPdata$drawdown[i] <- SPdata$water[i] / SPdata$highwater[i]
+    df$equity[i] <- df$open_pnl[i] + df$closed_pnl[i]
+    df$highwater[i] <- pmax(df$equity[i], df$highwater[i-1])
+    df$water[i] <- df$highwater[i] - df$equity[i]
+    df$drawdown[i] <- df$water[i] / df$highwater[i]
   }
 
   # make the summary trade table
-  trade_test <- sum(SPdata$signal)
+  trade_test <- sum(df$signal)
   if(trade_test == 0) {
-    buys <- SPdata |>
+    buys <- df |>
       select(on, buy_date, buy_price, buy_amount) |>
       filter(on == 1) |>
       select(!on)
-    sells <- SPdata |>
+    sells <- df |>
       select(off, sell_date:drawdown) |>
       filter(off == -1) |>
       select(!off)
@@ -149,13 +149,13 @@ for (j in seq_len(nrow(runs))) {
   }
 
   # return calculation
-  end_value <- SPdata$equity[nrow(SPdata)] ; end_val <- end_value / 1000000
+  end_value <- df$equity[nrow(df)] ; end_val <- end_value / 1000000
   ratio <- end_value/ start_value
-  start_date <- min(SPdata$date) ; end_date <- max(SPdata$date)
+  start_date <- min(df$date) ; end_date <- max(df$date)
   date_range <- as.numeric(difftime(end_date, start_date, units = "days")) / 365.25
   ICAGR <- if(ratio <= 0) 0 else log(ratio)/ date_range
-  drawdown <- max(SPdata$drawdown)
-  lake <- sum(SPdata$water) / sum(SPdata$equity)
+  drawdown <- max(df$drawdown)
+  lake <- sum(df$water) / sum(df$equity)
   bliss <- ICAGR / drawdown
   results[j,1:9] <- as_tibble_row(
     c(j=j, slow_lag=slow_lag, fast_lag=fast_lag, ICAGR=ICAGR, drawdown=drawdown,
@@ -170,22 +170,22 @@ secs <- forever  / nrow(runs)
 sprintf("Yo, %1.2f total time and %1.4f per run, %i runs", forever, secs, nrow(runs))
 
 # the promised land of pretty graphs
-SPdata |>        # S&P
+df |>        # S&P
   ggplot(aes(x = date, y = close)) +
   geom_line(linewidth = 1, alpha = 0.5)
 
 ggplot() +
-  geom_point(data=SPdata, aes(x=date, y=close), alpha=0.2) +
+  geom_point(data=df, aes(x=date, y=close), alpha=0.2) +
   geom_segment(data=trades, aes(x=buy_date, y=buy_price, xend=sell_date,
               yend=sell_price, size = 1, color="black"))
 
-SPdata |>        # lake
+df |>        # lake
   ggplot(aes(x = date)) +
   geom_ribbon(aes(ymin=equity, ymax=highwater, x=date, fill = "band"), alpha = 0.9)+
   scale_color_manual("", values="grey12")+
   scale_fill_manual("", values="red")
 
-SPdata |>       # equity and highwater
+df |>       # equity and highwater
   mutate(close_big = close*10500) |>
   ggplot(aes(x = date)) +
   geom_line(aes(y = equity),size = 1,  alpha = 0.9) +
@@ -195,7 +195,7 @@ SPdata |>       # equity and highwater
 # check for second scale
 # https://stackoverflow.com/questions/59956953/plotting-secondary-axis-using-ggplot
 
-SPdata |>
+df |>
   ggplot(aes(x = date)) +
   geom_line(aes(y = equity), size = 1, alpha = 0.8) +
   geom_line(aes(y = highwater), size=1, alpha=0.2)
